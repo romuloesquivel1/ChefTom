d6dff93f596688eee800ab4ad99f786c0e770c8e
Romulo Esquivel <romulo.esquivel1@outlook.com>
2023-11-09 03:13:39 +0000

    improve

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import '@popperjs/core'
+import 'bootstrap'
+import '@hotwired/turbo-rails'
+import 'controllers'
+
+window.document_ready = function (fn) {
+  if (document.readyState !== 'loading') {
+    fn();
+    return;
+  }
+  document.addEventListener('DOMContentLoaded', fn);
+}
